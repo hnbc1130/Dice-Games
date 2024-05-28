e0f2a5a2e36bf83c1de799daa997c3238da08679
HailedTomCat <Dylan.brown1130@gmail.com>
2024-05-28 14:40:44 -0400

    Moved game to setup file, fixed bugs

--- BasicLier.py
+++ BasicLier.py
@@ -1,22 +1,34 @@
-from GameFunctions.Make_players import players
-from GameFunctions.Make_bids import make_Bids
-from GameFunctions.winner import winner
+from GameFunctions.LierSetup.Make_players import players
+from GameFunctions.LierSetup.Make_bids import make_Bids
+from GameFunctions.LierSetup.winner import winner
+import GameFunctions.LierSetup.dice_set as dice_set
 
 class lier:
     def __init__(self, player_num):
         self.player_num = player_num 
         self.player = []
-
-
-    def game(self):
         for player in range(self.player_num):
             player = players()
             self.player.append(player)
 
-        make_Bids(self)
-        winner(self)
-        print("continue")
+    def game(self):
+        
+        try: 
+            while True:
+                make_Bids(self)
+                winner(self)
+                for player in self.player:
+                    if player.wins == 4:
+                        print(f"{player.name} has won")
+                        return False 
+                    else:
+                        player.dice_rolls = dice_set.dice_set(player.dice) 
+                        player.can_guess = True
+                        player.bids = (0, 0)
+                        continue
 
-#make a game file that just calls lier and allows the user to input number of players up to 4    
-#lier(3).game()
+                
+                #reset players back to default cleanup function?   
 
+        except:
+            EnvironmentError
--- PlayGame.py
+++ PlayGame.py
@@ -2,8 +2,9 @@ import BasicLier
 
 try:
 
-    #player_num = int(input("How many players?\n"))
-    BasicLier.lier(3).game()
+    player_num = int(input("How many players?\n"))
+    BasicLier.lier(player_num).game() #will use player_num instead of hardcode in the futurea
+    
         
 except:
     KeyError
