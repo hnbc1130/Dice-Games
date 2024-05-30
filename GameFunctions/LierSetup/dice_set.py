a1e4a7f65a86b96850691b5e3bcfba0beec496a3
HailedTomCat <Dylan.brown1130@gmail.com>
2024-05-30 18:10:17 -0400

    Functioning Game condensed folder, added init

--- GameFunctions/LierSetup/dice_set.py
+++
@@ -1,7 +0,0 @@
-import GameFunctions.LierSetup.diceroller as diceroller
-
-def dice_set(dice):
-    player_roll = [0] * dice
-    for roll in range(len(player_roll)):
-        player_roll[roll] = diceroller .diceroller(6).roll()
-    return player_roll
